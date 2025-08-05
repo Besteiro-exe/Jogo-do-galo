51827393c0c71e6a637c8440f5e15bfab76240d5
Besteiro-exe <pedrobesteirosilva@gmail.com>
2025-08-05 12:35:25 +0200

    Add files via upload

---
+++ jogo do galo gui.cpp
@@ -0,0 +1,16 @@
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int lado = 200
+
+
+int main(){
+    sf::RenderWindow window(sf::VideoMode(600, 600), "Jogo do Galo - SFML");
+    sf::Font fonte;
+    font.loadfromfile("arial.ttf");
+     
+}
